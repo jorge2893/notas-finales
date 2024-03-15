d772f374df968dc51db5a3c8d5c2ee5c893467c7
Jorge Solis <jorge.solis.sm@gmail.com>
2024-03-15 18:22:45 -0300

    se vuelven arreglar detalle

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,7 @@ var htmlnota1 = Number(prompt("Ingresa nota 1 de HTML"));
 var htmlnota2 = Number(prompt("Ingresa nota 2 de HTML"));
 var htmlnota3 = Number(prompt("Ingresa nota 3 de HTML"));
 
-var htmlpromedio = (htmlnota1 + htmlnota2 + htmlnota3)/3;
+var htmlpromedio = (htmlnota1 + htmlnota2 + htmlnota3) / 3;
 
 console.log("Promedio HTML: ", htmlpromedio);
 
@@ -10,7 +10,7 @@ var cssnota1 = Number(prompt("Ingresa nota 1 de CSS"));
 var cssnota2 = Number(prompt("Ingresa nota 2 de CSS"));
 var cssnota3 = Number(prompt("Ingresa nota 3 de CSS"));
 
-var csspromedio = (cssnota1nota1 + cssnota2nota2 + cssnota3nota3)/3;
+var csspromedio = (cssnota1 + cssnota2 + cssnota3) / 3;
 
 console.log("Promedio CSS: ", csspromediopromedio);
 
@@ -18,7 +18,7 @@ var javanota1 = Number(prompt("Ingresa nota 1 de JavaScript"));
 var javanota2 = Number(prompt("Ingresa nota 2 de JavaScript"));
 var javanota3 = Number(prompt("Ingresa nota 3 de JavaScript"));
 
-var javapromedio = (javanota1 + javanota2 + javanota3)/3;
+var javapromedio = (javanota1 + javanota2 + javanota3) / 3;
 
 console.log("Promedio JavaScript: ", javapromedio);
 
