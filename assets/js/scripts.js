a0381219417b834856806e051997fbd01fe86bf4
Jorge Solis <jorge.solis.sm@gmail.com>
2024-03-15 18:16:09 -0300

    se vuelven arreglar detalles

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,7 @@ var htmlnota2 = Number(prompt("Ingresa nota 2 de HTML"));
 var htmlnota3 = Number(prompt("Ingresa nota 3 de HTML"));
 
 var htmlpromedio = (htmlnota1 + htmlnota2 + htmlnota3)/3;
+
 console.log("Promedio HTML: ", htmlpromedio);
 
 var cssnota1 = Number(prompt("Ingresa nota 1 de CSS"));
@@ -10,6 +11,7 @@ var cssnota2 = Number(prompt("Ingresa nota 2 de CSS"));
 var cssnota3 = Number(prompt("Ingresa nota 3 de CSS"));
 
 var csspromedio = (cssnota1nota1 + cssnota2nota2 + cssnota3nota3)/3;
+
 console.log("Promedio CSS: ", csspromediopromedio);
 
 var javanota1 = Number(prompt("Ingresa nota 1 de JavaScript"));
@@ -17,23 +19,24 @@ var javanota2 = Number(prompt("Ingresa nota 2 de JavaScript"));
 var javanota3 = Number(prompt("Ingresa nota 3 de JavaScript"));
 
 var javapromedio = (javanota1 + javanota2 + javanota3)/3;
+
 console.log("Promedio JavaScript: ", javapromedio);
 
-document.getElementById("htmlnota1").innerText = htmlnota1.toFixed(2);
-document.getElementById("htmlnota2").innerText = htmlnota2.toFixed(2);
-document.getElementById("htmlnota3").innerText = htmlnota3.toFixed(2);
-document.getElementById("htmlpromedio").innerText = htmlpromedio.toFixed(2);
+document.getElementById("htmlnota1tabla").innerText = htmlnota1.toFixed(2);
+document.getElementById("htmlnota2tabla").innerText = htmlnota2.toFixed(2);
+document.getElementById("htmlnota3tabla").innerText = htmlnota3.toFixed(2);
+document.getElementById("htmlpromediotabla").innerText = htmlpromedio.toFixed(2);
 
 
-document.getElementById("cssnota1").innerText = cssnota1.toFixed(2);
-document.getElementById("cssnota2").innerText = cssnota2.toFixed(2);
-document.getElementById("cssnota3").innerText = cssnota3.toFixed(2);
-document.getElementById("cssnpromedio").innerText = csspromedio.toFixed(2);
+document.getElementById("cssnota1tabla").innerText = cssnota1.toFixed(2);
+document.getElementById("cssnota2tabla").innerText = cssnota2.toFixed(2);
+document.getElementById("cssnota3tabla").innerText = cssnota3.toFixed(2);
+document.getElementById("cssnpromediotabla").innerText = csspromedio.toFixed(2);
 
-document.getElementById("javanota1").innerText = javanota1.toFixed(2);
-document.getElementById("javanota2").innerText = javanota2.toFixed(2);
-document.getElementById("javanota3").innerText = javanota3.toFixed(2);
-document.getElementById("javapromedio").innerText = javapromedio.toFixed(2);
+document.getElementById("javanota1tabla").innerText = javanota1.toFixed(2);
+document.getElementById("javanota2tabla").innerText = javanota2.toFixed(2);
+document.getElementById("javanota3tabla").innerText = javanota3.toFixed(2);
+document.getElementById("javapromediotabla").innerText = javapromedio.toFixed(2);
 
 var promediogeneral = (htmlpromedio + csspromedio + javapromedio) / 3;
-document.getElementById("promediogeneral").innerText = promediogeneral.toFixed(2);
+document.getElementById("promediogeneraltabla").innerText = promediogeneral.toFixed(2);
